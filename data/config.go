5c0671d5e8a95bb2a22af43ad6154ce25c4fe974
Randall Smith <perlstalker@gmail.com>
2014-03-08 20:09:19 -0700

    Now pull config options from data store

---
+++ data/config.go
@@ -0,0 +1,82 @@
+package data
+
+import (
+	"appengine"
+	"appengine/datastore"
+	"log"
+)
+
+/*
+ * K/V values:
+ *  lastCheck Time (as string)
+ *  bookmarkUrl string
+ *  bookmarkService string (default to delicious)
+ */
+type KVPair struct {
+	Key string;
+	Value string;
+}
+
+func configOptionKey (c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "KVPair", "config_options", 0, nil)
+}
+
+func GetConfigOption (c appengine.Context, key string) (value string) {
+	q := datastore.NewQuery("KVPair").
+		Filter("Key = ", key);
+	results := q.Run(c)
+	for {
+		var kv KVPair
+		_, err := results.Next(&kv);
+		if err == datastore.Done {
+			break // we're done
+		}
+		if err != nil {
+			log.Fatal("error fetching values: ", err)
+			break
+		}
+		value = kv.Value
+	}
+	return value
+}
+
+func SetConfigOption (c appengine.Context, key string, value string) {
+	q := datastore.NewQuery("KVPair").
+		Filter("Key = ", key);
+	results := q.Run(c)
+
+	db_entry := datastore.NewIncompleteKey(c, "KVPair", configOptionKey(c));
+
+	var kv KVPair
+	for {
+		db_key, err := results.Next(&kv);
+		if err == datastore.Done {
+			break // the result was not found
+		}
+		if err != nil {
+			log.Fatal("error fetching values: %v", err)
+			break
+		}
+		db_entry = db_key
+	}
+	kv.Key   = key;
+	kv.Value = value;
+
+	_, err := datastore.Put(c, db_entry, &kv);
+	if err != nil {
+		log.Fatal("error putting option: ", err);
+	}
+}
+
+func GetAllConfigOptions (c appengine.Context) (options []KVPair) {
+	q := datastore.NewQuery("KVPair").
+		Order("Key");
+
+	_, err := q.GetAll(c, &options);
+
+	if err != nil {
+		log.Fatal("error fetching options: ", err);
+	}
+
+	return options
+}
